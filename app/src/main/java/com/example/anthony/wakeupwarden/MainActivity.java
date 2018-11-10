c0ea43b5788a003b3c4569d8a692c5e2e002531d
dderluk <44528289+dderluk@users.noreply.github.com>
2018-11-09 19:52:07 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/anthony/wakeupwarden/MainActivity.java
+++ app/src/main/java/com/example/anthony/wakeupwarden/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.anthony.wakeupwarden;
 
-// code taken from moodle and modified
-import android.app.AlarmManager;
-import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,14 +10,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TimePicker;
 import android.app.TimePickerDialog;
-import android.view.ContextMenu;
-import android.widget.Toast;
 
-import com.example.danie.wakeupwarden.Database.DatabaseHelper;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
@@ -30,27 +30,25 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
     private static final String TAG = "MainActivity";
     protected FloatingActionButton insertAlarmButton;
     protected ListView alarmsListView;
-    String time;
 
 
 
 
-    List<Alarm> alarms = null;
-    List<Alarm> alarmsId = null;
+
+//Array Alarm used to store the alarms in the database
+    ArrayList<Alarm> Alarm = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //defining the views in the xml
         setContentView(R.layout.activity_main);
-
         alarmsListView = findViewById(R.id.AlarmListView);
-
         insertAlarmButton = findViewById(R.id.InsertAlarmButton);
 
 
-
-        loadListView();
-
+        // opens alarm dialog to set the alarm
         insertAlarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,26 +61,21 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
         });
 
 
-        alarmsListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-
+        // opens edit alarm dialog
         alarmsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
            @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               DatabaseHelper dbhelper = new DatabaseHelper(MainActivity.this);
-
-               alarmsId = dbhelper.getAllAlarms();//get all courses from database
-               ArrayList<Long> listAlarmsId = new ArrayList<>(); // dummy array used to view list
-
-               for (int i = 0; i < alarms.size(); i++) {
-                   listAlarmsId.add(alarms.get(i).getId());
-               }
 
 
-
+               //opens the dialog
                EditListItemDialog dialog = new EditListItemDialog();
                dialog.show(getSupportFragmentManager(), "EditAlarmDialog");
+               // sends the id of the alarm to the dialogue in a bundle
                Bundle bundle = new Bundle();
-               bundle.putString("alarmId", String.valueOf(listAlarmsId.get(position)));
+               //Alarm.get(position).getId())) gets the id of the alarm based on its position in the listView which corresponds to its index in the array
+               bundle.putString("alarmId", String.valueOf(Alarm.get(position).getId()));
+
+
                // set MyFragment Arguments
                dialog.setArguments(bundle);
 
@@ -92,59 +85,102 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
 
         });
 
+        //sets the wakeupwarden database reference
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("alarms");
+        //addListenerForSingleValueEvent: Used to retrieve the values in the database for a single instance at the given reference
+        ref.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //dataSnapshot stores the data retrieved (the entire database)
+                        //loadListView adapts the data and displays the data on the ListView
+                        loadListView((Map<String,Object>) dataSnapshot.getValue());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+        //value even Listener used to update the list when a value is changed in the database
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //loadListView adapts the data and updates the data on the ListView
+                loadListView((Map<String,Object>) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        ref.addValueEventListener(listener);
     }
 
-
+// needed function to retrieve the data from the time set dialog
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         //store values in string
         //TextView text = (TextView)findViewById(R.id.text);
+        String time;
         if(minute < 10){
             time =  String.valueOf(hourOfDay) +":0" + String.valueOf(minute);
         }
         else
             time =  String.valueOf(hourOfDay) +":" + String.valueOf(minute);
-        DatabaseHelper dbhelper = new DatabaseHelper(this);
-        dbhelper.insertAlarm(new Alarm(-1, "", time, true));
-        loadListView();
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
-        c.set(Calendar.MINUTE, minute);
-        c.set(Calendar.SECOND, 0);
-        //updateTime(c);
-       // startAlarm(c);
+
+        DatabaseReference databaseAlarms;
+        //sets the wakeupwarden database reference alarm
+        databaseAlarms = FirebaseDatabase.getInstance().getReference("alarms");
+        // gets a key that is not used
+        String key = databaseAlarms.push().getKey();
+        //saves the alarm to the database with the key generated above
+        //NOTE: ID OF THE ALARM IS THE KEY GENERATED
+        databaseAlarms.child(key).setValue(new Alarm(key, "", time, true));
+        //NOTE valueEventListener will realize this change in database
+
 
     }
 
 
+// loads the list View
+    protected void loadListView(Map<String,Object> alarms) {
+        // dummy list used to display the alarm list
+        ArrayList<String> alarmList = new ArrayList<>();
+        // dummy List to set the global array Alarm to the same order as the alarmList displayed on the user interface
+        ArrayList<Alarm> AlarmList = new ArrayList<>();
 
-    protected void loadListView() {
-        DatabaseHelper dbhelper = new DatabaseHelper(this);
-        alarms = dbhelper.getAllAlarms();//get all courses from database
-        ArrayList<String> listAlarms = new ArrayList<>(); // dummy array used to view list
+        //iterate through each alarm
+        for (Map.Entry<String, Object> entry : alarms.entrySet()){
 
-        for (int i = 0; i < alarms.size(); i++) {
-                listAlarms.add(alarms.get(i).getTime());
+            //Get alarm map
+            Map alarmEntry = (Map) entry.getValue();
+            //Get alarm time
+            alarmList.add((String) alarmEntry.get("time"));
 
-            }
-                        //send the list to the adatper to be viewed by the list view
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listAlarms);
+            //adds the an alarm to the alarm list
+            AlarmList.add(new Alarm((String) alarmEntry.get("id"), (String) alarmEntry.get("title"), (String) alarmEntry.get("time"),(Boolean) alarmEntry.get("enable")));
+        }
+        //sets the Alarm array to the correspond in the same order as the alarm list
+        Alarm = AlarmList;
+
+
+
+        //send the list to the adapter to be viewed by the list view
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alarmList);
         alarmsListView.setAdapter(adapter);
+
+
+
+
 
         }
 
 
 
-/*
-    private void startAlarm(Calendar c) {
 
-        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-        Intent intent= new Intent(this, AlertReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
-        //initalize alarm
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
-    }
-*/
 
     }
 
